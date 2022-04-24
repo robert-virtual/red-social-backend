e3ba90e1b8fd03c1574c7110071f934b6e836230
robert-virtual <robertocastill63@gmail.com>
2022-04-23 18:43:52 -0600

    parte 4

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,6 @@ const port = process.env.PORT || 3000;
 // middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
 
 app.use("/", authRouter);
 app.use("/posts", postsRouter);
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -5,34 +5,29 @@ import { auth } from "../middlewares/auth";
 import { upload } from "../helpers/upload-s3";
 const prisma = new PrismaClient();
 
-router.post("/ktor", upload.array("images"),(req, res) => {
-    console.log(req.files)
-  res.json({msg:"hello"});
-});
-
-
 router.get("/", (req, res) => {
   res.json({});
 });
 
-router.post("/", auth, upload.array("images"), async (req, res) => {
+router.post("/", upload.array("images"), async (req, res) => {
   let imagenes: { url: string }[] = [];
-  console.log(req.body.images);
-
+  console.log("body:", req.body);
   if (req.files) {
+    console.log("Si hay archivos", req.files.length);
     let arr = req.files as Express.MulterS3.File[];
     arr.forEach((f) => {
       imagenes.push({ url: f.location });
     });
+    console.log("imagenes", imagenes);
   }
-
+  const user = await prisma.user.findFirst();
   const post = await prisma.post.create({
     data: {
       images: {
         create: imagenes,
       },
       content: req.body.content,
-      userId: req.userId!,
+      userId: req.userId ?? user!.id,
     },
   });
 
